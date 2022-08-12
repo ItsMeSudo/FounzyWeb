ea8c157565d204da271a4c673725a236d3fe4aeb
SUDO <82466939+ItsMeSudo@users.noreply.github.com>
2022-08-12 22:09:02 +0200

    1.0

---
+++ dockerfile
@@ -0,0 +1,18 @@
+#ProjektZ server
+#SUDO              2022            SERVERHU.EU
+
+FROM ubuntu:20.04
+VOLUME [ "/data" ]
+WORKDIR /data
+ENV TZ=Europe/Budapest
+ARG DEBIAN_FRONTEND=noninteractive
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+RUN apt update -y && \
+    apt install git wget -y && \
+    cd /root && \
+    git clone https://github.com/ItsMeSudo/FounzyWeb.git && \
+    cd FounzyWeb && chmod +x * && \
+    ./setup.sh
+
+CMD startup; sleep infinity
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SUDO                 2022              SERVERHU.EU
+
+cd /root && \
+    apt install -y nginx unzip wget curl git && \
+    unlink /etc/nginx/sites-enabled/default && \
+    cd /root/FounzyWeb && \
+    sed -i 's/\r//' /root/FounzyWeb/gitclone && \
+    sed -i 's/\r//' /root/FounzyWeb/permchk && \
+    sed -i 's/\r//' /root/FounzyWeb/update && \
+    sed -i 's/\r//' /root/FounzyWeb/startup && \
+    mv /root/FounzyWeb/startup /usr/bin && \
+    mv /root/FounzyWeb/gitclone /usr/bin && \
+    mv /root/FounzyWeb/permchk /usr/bin && \
+    mv /root/FounzyWeb/update /usr/bin && \
+    mkdir /FounzyCONF && \
+    mv /root/FounzyWeb/Founzy.conf /FounzyCONF && \
+    mv /root/FounzyWeb/index.html /FounzyCONF && \
+    rm dockerfile && \
+    ln -s /FounzyCONF/Founzy.conf /etc/nginx/sites-available/Founzy.conf && \
+    ln -s /FounzyCONF/Founzy.conf /etc/nginx/sites-enabled/Founzy.conf
